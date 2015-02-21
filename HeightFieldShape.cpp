8e00aeb16f2c82935a8a28cc5e3b5a49a219f229
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-02-21 18:23:35 +0100

    Objet coordonnées + récupération bonne zone
    Utilisation std::pair<int, int> pour les coordonnées et récupération des
    valeurs de hauteur correspondant à la bonne zone de terrain.

--- HeightFieldShape.cpp
+++ HeightFieldShape.cpp
@@ -3,14 +3,34 @@
 #include "GestionnaireTerrain.h"
 #include <OgreTerrainGroup.h>
 
-HeightFieldShape::HeightFieldShape(hkpSampledHeightFieldBaseCinfo const& rInfo, GestionnaireTerrain const *pTerrains, Ogre::Vector2 cooHeightField): hkpSampledHeightFieldShape(rInfo)
+HeightFieldShape::HeightFieldShape(hkpSampledHeightFieldBaseCinfo const& rInfo, GestionnaireTerrain const *pTerrains, std::pair<int, int> coo): hkpSampledHeightFieldShape(rInfo)
 {
-	mCooHeightField = cooHeightField;
-	mpHeightData = pTerrains->getTerrains()->getTerrain(0, 0)->getHeightData();	//TODO ne récupérer qu'un chunk de 500 sur 500
+	mpHeightData = new float*[TAILLE_CHUNK];
+
+	for (int i=0; i<TAILLE_CHUNK; ++i)
+	{
+		mpHeightData[i] = new float[TAILLE_CHUNK];
+	}
+
+	for (int i=0; i<TAILLE_CHUNK; ++i)
+	{
+		for (int j=0; j<TAILLE_CHUNK; ++j)
+		{
+			mpHeightData[i][j] = pTerrains->getTerrains()->getTerrain(0,0)->getHeightAtWorldPosition(coo.first*TAILLE_CHUNK +i, 5000.0f, coo.second*TAILLE_CHUNK +j);
+		}
+	}
 }
 
 HeightFieldShape::~HeightFieldShape()
 {
+	for (int i=0; i<TAILLE_CHUNK; ++i)
+	{
+		delete[] mpHeightData[i];
+		mpHeightData[i] = nullptr;
+	}
+
+	delete[] mpHeightData;
+	mpHeightData = nullptr;
 }
 
 HK_FORCE_INLINE hkBool HeightFieldShape::getTriangleFlipImpl() const 
@@ -25,5 +45,5 @@ void HeightFieldShape::collideSpheres(CollideSpheresInput const& input, SphereCo
 
 HK_FORCE_INLINE hkReal HeightFieldShape::getHeightAtImpl(int x, int z) const 
 {
-	return mpHeightData[x*TAILLE_CHUNK+z] / ((float)TAILLE_MONDE/(float)TAILLE_CHUNK);
+	return mpHeightData[x][z];
 }
