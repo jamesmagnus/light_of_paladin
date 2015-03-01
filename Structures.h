6a58bcdb88902b0b642f22eb3a7dfc6ac03c1403
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-03-01 22:41:18 +0100

    Réduction taillle chunk + changement structure
    Taille d'un chunk 500 -> 100
    Ajout de l'offset dans la structure
    Changement du type de pointeurs dans la structure

--- Structures.h
+++ Structures.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #define TAILLE_MONDE 50000
-#define TAILLE_CHUNK 500
+#define TAILLE_CHUNK 100
 #define TAILLE_IMG_HEIGHTMAP 2049
 
 #include <OgreVector3.h>
 
-class hkpRigidBody;
+class Chunk;
 
 typedef struct sTableauChunks
 {
-	hkpRigidBody* ppChunk[9];
+	Chunk* ppChunk[3][3];
 	Ogre::Vector3 positionJoueur;
+	std::pair<int, int> offset;
 } TableauChunks;
