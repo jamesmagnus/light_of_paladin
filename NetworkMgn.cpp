627d258680d2933217f28ca58d08ca7b24dda1c8
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-03-14 18:30:11 +0100

    Ajout de la classe gestionnaire réseau

---
+++ NetworkMgn.cpp
@@ -0,0 +1,31 @@
+#include "NetworkMgn.h"
+
+#include <RakPeerInterface.h>
+#include <MessageIdentifiers.h>
+#include <RakVoice.h>
+
+#define SERVER_PORT 0
+#define MAX_CLIENTS 60000
+
+
+NetworkMgn::NetworkMgn()
+{
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+
+	RakNet::SocketDescriptor socketDscrp(SERVER_PORT,0);
+	RakNet::Packet *pPacket;
+
+	peer->Startup(MAX_CLIENTS, &socketDscrp, 1);
+	peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+
+	/*RakNet::RakVoice voice;
+	voice.Init(16000u, 2*16000u);*/
+	
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+}
+
+
+NetworkMgn::~NetworkMgn()
+{
+}
---
+++ NetworkMgn.h
@@ -0,0 +1,8 @@
+#pragma once
+class NetworkMgn
+{
+public:
+	NetworkMgn();
+	~NetworkMgn();
+};
+
