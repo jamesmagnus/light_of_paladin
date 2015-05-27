61d1a4574c20094c276d81b83e9a3a11e91ebab1
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-05-27 18:46:00 +0200

    Ajout de la classe Chaussure

---
+++ ClassChaussure.cpp
@@ -0,0 +1,11 @@
+﻿#include "ClassChaussure.h"
+
+Chaussure::Chaussure(Ogre::SceneNode *pNode, int prix, float poid, std::string const& nom, bool IsUnique, bool IsVisible, Sort *pEnchantement):Vetement(pNode, prix, poid, nom, IsUnique, IsVisible, pEnchantement)
+{
+
+}
+
+
+Chaussure::~Chaussure(void)
+{
+}
---
+++ ClassChaussure.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "ClassVetement.h"
+
+class Chaussure :public Vetement
+{
+public:
+
+	/* Constructeur */
+	/* pNode, l'adresse du noeud de Ogre */
+	/* prix, poid, nom, caractéristiques de la chaussure */
+	/* IsUnique, true si la chaussure ne peut se trouver 2 fois dans un même inventaire, false par défaut */
+	/* IsVisible, true si la chaussure doit être rendu par Ogre */
+	/* pEnchantement, pointeur vers un sort qui constitue l'enchantement des chaussures ou nullptr si pas d'enchantement */
+	Chaussure(Ogre::SceneNode *pNode, int prix=0, float poid=1.0, std::string const& nom="defaultShoes", bool IsUnique=false, bool IsVisible=true, Sort *pEnchantement=nullptr);
+
+	/* Destructeur */
+	virtual ~Chaussure();
+};
+
