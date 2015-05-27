c1de731086df3deebedfc00db51a928b4c424bbd
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-05-27 18:37:52 +0200

    Ajout du gestionnaire sonore FMOD

---
+++ FMODSoundMgr.cpp
@@ -0,0 +1,62 @@
+﻿#include "FMODSoundMgr.h"
+#include "ExceptionPerso.h"
+
+#include <fmod_errors.h>
+#include <string>
+
+using namespace FMOD;
+
+FMODSoundMgr::FMODSoundMgr(): Ogre::FrameListener()
+{
+	pSys = nullptr;
+
+	FMOD_RESULT res = System_Create(&pSys);
+
+	if (res != FMOD_OK)
+	{
+		throw ExceptionPerso((std::string("Erreur lors de la création du système FMOD: ") + FMOD_ErrorString(res)).c_str(), FATAL);
+	}
+
+	res = pSys->init(1024, FMOD_INIT_3D_RIGHTHANDED, nullptr);
+
+	if (res != FMOD_OK)
+	{
+		throw ExceptionPerso((std::string("Erreur lors de l'initialisation du système FMOD: ") + FMOD_ErrorString(res)).c_str(), FATAL);
+	}
+}
+
+
+FMODSoundMgr::~FMODSoundMgr()
+{
+}
+
+bool FMODSoundMgr::frameStarted(Ogre::FrameEvent const& evt)
+{
+	pSys->update();
+
+	return true;
+}
+
+bool FMODSoundMgr::loadSound(std::string const& fichier, std::string const& nom)
+{
+	Sound *ps;
+	pSys->createStream(fichier.c_str(), 0, nullptr, &ps);
+
+	std::pair<std::map<std::string, Sound*>::iterator, bool> ret = mSoundPtrMap.insert(make_pair(nom, ps));
+
+	return ret.second;
+}
+
+void FMODSoundMgr::playLoadedSound(std::string const& nom, bool const paused) const
+{
+	Sound *ps = mSoundPtrMap.at(nom);
+
+	if (ps != nullptr)
+	{
+		pSys->playSound(ps, nullptr, paused, nullptr);
+	}
+	else
+	{
+		throw ExceptionPerso((std::string("Impossible de lire ") + nom).c_str(), ERREUR);
+	}
+}
---
+++ FMODSoundMgr.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include <fmod.hpp>
+
+#include <OgreFrameListener.h>
+
+#include <boost/noncopyable.hpp>
+
+#include <map>
+
+/* Classe utilisée pour gérer FMOD et tout le système sonore du jeu */
+/* Classe finale, non dérivable */
+/* Sémantique d'entité, hérite de boost::noncopyable */
+/* Hérite de Ogre::FrameListener, doit être ajoutée à la liste des listers de Ogre */
+class FMODSoundMgr: public Ogre::FrameListener, private boost::noncopyable
+{
+private:
+	FMOD::System *pSys;
+	std::map<std::string, FMOD::Sound*> mSoundPtrMap;
+
+public:
+
+	/* Constructeur */
+	/* Lève une exception en cas de problème */
+	FMODSoundMgr();
+
+	/* Destructeur */
+	virtual ~FMODSoundMgr();
+
+	/* Appelé par Ogre à chaque début de frame */
+	virtual bool frameStarted(Ogre::FrameEvent const& evt) override;
+
+	/* Charge un son */
+	/* renvoie true si le son est chargé, false sinon */
+	/* fichier, le nom du fichier à charger */
+	/* nom, le nom associé au son */
+	bool loadSound(std::string const& fichier, std::string const& nom);
+
+	/* Lance la lecture du son */
+	/* nom, le nom du son à lancer */
+	/* Lève une exception si le son n'existe pas */
+	void playLoadedSound(std::string const& nom, bool const paused=false) const;
+
+	/* Décharge un son */
+	/* nom, le nom du son à décharger */
+	/* Lève une exception si le son est introuvable */
+	void unloadSound(std::string const& nom);
+};
+
