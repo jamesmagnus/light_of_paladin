712dac7d5c124fd2882b749cdb89d8daf566bef6
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-06-17 23:48:21 +0200

    Header perso + changement frame event

--- FMODSoundMgr.cpp
+++ FMODSoundMgr.cpp
@@ -1,4 +1,5 @@
-﻿#include "FMODSoundMgr.h"
+﻿#include "StdLibAndNewOperator.h"
+#include "FMODSoundMgr.h"
 #include "ExceptionPerso.h"
 
 #include <fmod_errors.h>
@@ -30,7 +31,7 @@ FMODSoundMgr::~FMODSoundMgr()
 {
 }
 
-bool FMODSoundMgr::frameStarted(Ogre::FrameEvent const& evt)
+bool FMODSoundMgr::frameRenderingQueued(Ogre::FrameEvent const& evt)
 {
 	pSys->update();
 
@@ -39,23 +40,43 @@ bool FMODSoundMgr::frameStarted(Ogre::FrameEvent const& evt)
 
 bool FMODSoundMgr::loadSound(std::string const& fichier, std::string const& nom)
 {
-	Sound *ps;
-	pSys->createStream(fichier.c_str(), 0, nullptr, &ps);
+	Sound *pS;
 
-	std::pair<std::map<std::string, Sound*>::iterator, bool> ret = mSoundPtrMap.insert(make_pair(nom, ps));
+	pSys->createStream(fichier.c_str(), 0, nullptr, &pS);
+
+	std::pair<std::map<std::string, std::pair<Sound*, Channel*>>::iterator, bool> ret = mSoundPtrMap.insert(std::make_pair(nom, std::make_pair(pS, nullptr)));
 
 	return ret.second;
 }
 
-void FMODSoundMgr::playLoadedSound(std::string const& nom, bool const paused) const
+void FMODSoundMgr::playLoadedSound(std::string const& nom, bool const paused)
 {
-	Sound *ps = mSoundPtrMap.at(nom);
-
-	if (ps != nullptr)
+	try
 	{
-		pSys->playSound(ps, nullptr, paused, nullptr);
+		Sound *pS = mSoundPtrMap.at(nom).first;
+		pSys->playSound(pS, nullptr, paused, &mSoundPtrMap.at(nom).second);
 	}
-	else
+	catch(...)
+	{
+		throw ExceptionPerso((std::string("Impossible de lire ") + nom).c_str(), ERREUR);
+	}
+}
+
+bool FMODSoundMgr::stopSound(std::string const& nom) const
+{
+	try
+	{
+		Sound *pS = mSoundPtrMap.at(nom).first;
+		Channel *pC = mSoundPtrMap.at(nom).second;
+
+		bool ret;
+		pC->isPlaying(&ret);
+
+		pC->stop();
+
+		return ret;
+	}
+	catch(...)
 	{
 		throw ExceptionPerso((std::string("Impossible de lire ") + nom).c_str(), ERREUR);
 	}
