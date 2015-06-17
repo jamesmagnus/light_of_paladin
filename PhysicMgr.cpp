f2520777ccaf219af14d0ee21d6a2901c885645d
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-06-18 00:06:49 +0200

    Ajout gestionnaire de physique Bullet

---
+++ PhysicMgr.cpp
@@ -0,0 +1,57 @@
+﻿#include "StdLibAndNewOperator.h"
+#include "PhysicMgr.h"
+
+#include <btBulletDynamicsCommon.h>
+#include <OgreLogManager.h>
+
+
+PhysicMgr::PhysicMgr()
+{
+	mpCollisionDispatch = nullptr;
+	mpCollisionCfg = nullptr;
+	mpBroadphase = nullptr;
+	mpConstraintSolv = nullptr;
+	mpWorld = nullptr;
+}
+
+
+PhysicMgr::~PhysicMgr()
+{
+	destroyBullet();
+}
+
+bool PhysicMgr::initBullet()
+{
+	Ogre::LogManager::getSingletonPtr()->logMessage("**** Init Bullet ****");
+
+	try
+	{
+		mpCollisionCfg = new btDefaultCollisionConfiguration();
+		mpCollisionDispatch = new btCollisionDispatcher(mpCollisionCfg);
+		mpBroadphase = new btDbvtBroadphase();
+		mpConstraintSolv = new btSequentialImpulseConstraintSolver();
+		mpWorld = new btDiscreteDynamicsWorld(mpCollisionDispatch, mpBroadphase, mpConstraintSolv, mpCollisionCfg);
+	}
+	catch (std::bad_alloc const& e)
+	{
+		Ogre::LogManager::getSingletonPtr()->logMessage("**** Bullet failed to allocate memory ****");
+		return false;
+	}
+
+	Ogre::LogManager::getSingletonPtr()->logMessage("**** Bullet successfully initialized ****");
+
+	mpWorld->setGravity(btVector3(0.0, -9.81, 0.0));
+
+	Ogre::LogManager::getSingletonPtr()->logMessage("[Bullet] Setting gravity to -9.81");
+
+	return true;
+}
+
+void PhysicMgr::destroyBullet()
+{
+	delete mpWorld;
+	delete mpBroadphase;
+	delete mpConstraintSolv;
+	delete mpCollisionDispatch;
+	delete mpCollisionCfg;
+}
---
+++ PhysicMgr.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <boost/core/noncopyable.hpp>
+
+class btDefaultCollisionConfiguration;
+class btCollisionDispatcher;
+class btDiscreteDynamicsWorld;
+class btBroadphaseInterface;
+class btSequentialImpulseConstraintSolver;
+
+/* Classe pour gérer le moteur de physique du jeu (Bullet) */
+/* Sémantique d'entité hérite de boost::noncopyable */
+/* Classe finale non dérivable*/
+class PhysicMgr final: boost::noncopyable
+{
+public:
+
+	/* Constructeur */
+	PhysicMgr();
+
+	/* Destructeur */
+	~PhysicMgr();
+
+	/* Initialise Bullet */
+	/* Renvoie true si OK, false sinon*/
+	bool initBullet();
+
+	/* Libère les ressources allouées pour Bullet */
+	void destroyBullet();
+
+private:
+	btDefaultCollisionConfiguration *mpCollisionCfg;
+	btCollisionDispatcher *mpCollisionDispatch;
+	btDiscreteDynamicsWorld *mpWorld;
+	btBroadphaseInterface *mpBroadphase;
+	btSequentialImpulseConstraintSolver *mpConstraintSolv;
+};
+
