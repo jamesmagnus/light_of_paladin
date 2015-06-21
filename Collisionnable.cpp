7cd13fc0c0615774a628700097ebd43e8f9a3740
Jean-Loup Beaussart (jamesmagnus) <beaussartjl@laposte.net>
2015-06-21 03:02:20 +0200

    Ajout classe gestion d'un objet physique

--- Collisionnable.cpp
+++ Collisionnable.cpp
@@ -1,11 +1,88 @@
 ﻿#include "Collisionnable.h"
 
+#include <btBulletDynamicsCommon.h>
 
-Collisionnable::Collisionnable()
+Collisionnable::Collisionnable(EShape shapeType)
 {
+	mpTransform = new btTransform();
+	mpBody = nullptr;
+	mpMotionState = nullptr;
+	mMasse = 0.0;
+	mInertie = btVector3(0.0, 0.0, 0.0);
+
+	mType = shapeType;
+
+	switch (shapeType)
+	{
+	case EShape::BOX:
+		mpCollShape = new btBoxShape(btVector3(0.0, 0.0, 0.0));
+		break;
+	case EShape::SPHERE:
+		mpCollShape = new btSphereShape(0.0);
+	case EShape::CAPSULE:
+		mpCollShape = new btCapsuleShape(0.0, 0.0);
+		break;
+	case EShape::CYLINDER:
+		mpCollShape = new btCylinderShape(btVector3(0.0, 0.0, 0.0));
+		break;
+	case EShape::CONE:
+		mpCollShape = new btConeShape(0.0, 0.0);
+		break;
+	case EShape::MULTI_SPHERES:
+		mpCollShape = new btMultiSphereShape(nullptr, nullptr, 0);
+		break;
+	default:
+		mpCollShape = new btBoxShape(btVector3(0.0, 0.0, 0.0));
+		break;
+	}
+
 }
 
 
 Collisionnable::~Collisionnable()
 {
+	delete mpTransform;
+	mpTransform = nullptr;
+
+	delete mpCollShape;
+	mpCollShape = nullptr;
+
+	delete mpMotionState;
+	mpMotionState = nullptr;
+
+	delete mpBody;
+	mpBody = nullptr;
+}
+
+void Collisionnable::calcInertie()
+{
+	if (mMasse != 0.0)
+	{
+		mpCollShape->calculateLocalInertia(mMasse, mInertie);
+	}
+}
+
+bool Collisionnable::createMotionState()
+{
+	if (mpMotionState == nullptr)
+	{
+		mpMotionState = new btDefaultMotionState(*mpTransform);
+		return false;
+	}
+
+	return true;
+}
+
+bool Collisionnable::createBody()
+{
+	if (mpBody == nullptr)
+	{
+		btRigidBody::btRigidBodyConstructionInfo const c(mMasse, mpMotionState, mpCollShape, mInertie);
+
+		mpBody = new btRigidBody(c);
+
+		return false;
+	}
+
+	return true;
 }
--- Collisionnable.h
+++ Collisionnable.h
@@ -1,11 +1,76 @@
 ﻿#pragma once
 
+#include "enumerations.h"
+
+#include <LinearMath/btScalar.h>
+#include <LinearMath/btVector3.h>
+#include <LinearMath/btDefaultMotionState.h>
+
+
+class btCollisionShape;
+class btTransform;
+class btRigidBody;
+class btMotionState;
+
 /* Classe de base pour les objets dont la physique doit être gérée */
 /* Sémantique d'entité hérite de boost::noncopyable */
 class Collisionnable
 {
 public:
-	Collisionnable();
+
+	/* Constructeur */
+	/* shapeType, enum Shape qui détermine quel forme est utilisée pour représenter l'objet */
+	Collisionnable(EShape shapeType);
+
+	/* Destructeur */
 	virtual ~Collisionnable();
+
+	/* Renvoie une référence sur la matrice de transformation Bullet */
+	 inline btTransform const& getTransform() const;
+
+	 /* Défini la masse */
+	 inline void setMasse(btScalar masse);
+
+	 /* Calcul l'inertie */
+	 void calcInertie();
+
+	 /* Génère le motionState */
+	 /* Renvoie true si le motionState existait déjà, false sinon */
+	 bool createMotionState();
+
+	 /* Crée le body */
+	 /* Renvoie true si le body était déjà crée, false sinon */
+	 bool createBody();
+
+	 /* Renvoie l'adresse du body */
+	 inline btRigidBody const * getBodyPtr() const;
+
+
+protected:
+	btCollisionShape *mpCollShape;
+	btTransform *mpTransform;
+	btVector3 mInertie;
+	btScalar mMasse;
+	btMotionState *mpMotionState;
+	btRigidBody *mpBody;
+
+	EShape mType;
 };
 
+inline btTransform const& Collisionnable::getTransform() const
+{
+	return *mpTransform;
+}
+
+inline void Collisionnable::setMasse(btScalar masse)
+{
+	if (masse >= 0.0)
+	{
+		mMasse = masse;
+	}
+}
+
+inline btRigidBody const* Collisionnable::getBodyPtr() const
+{
+	return mpBody;
+}
